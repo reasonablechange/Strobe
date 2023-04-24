755fc990372a67c3dfccb40205809b5f45b2a614
reasonablechange <90572736+reasonablechange@users.noreply.github.com>
2023-04-24 18:19:54 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Strobe',
+    version='1.0.3 release',
+    packages=[''],
+    url='',
+    license='',
+    author='Mirage Technology',
+    author_email='',
+    description=''
+)
